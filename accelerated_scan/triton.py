76bbb9d20a5d01486bca361f9e121d26e0728dcb
Vol Kyrylov <vol@wilab.org.ua>
2025-12-23 22:56:06 -0800

    use tl.associative_scan with tuple inputs

--- accelerated_scan/triton.py
+++ accelerated_scan/triton.py
@@ -2,38 +2,15 @@ import torch
 import triton
 import triton.language as tl
 
-assert triton.__version__ != '2.1.0', 'Triton 2.1.0 is missing enable_fp_fusion. Triton 2.2.0 is required for numerical stability of this implementation.'
-
-
-# manual tuple packing by @jackd from https://github.com/openai/triton/issues/2359
-@triton.jit
-def unpack64(merged):
-    tl.static_assert(merged.dtype == tl.uint64)
-    b = (merged & 0xFFFFFFFF).to(tl.uint32).to(tl.float32, bitcast=True)
-    a = (merged >> 32).to(tl.uint32).to(tl.float32, bitcast=True)
-    return a, b
-
 
 @triton.jit
-def pack64(a, b):
-    tl.static_assert(a.dtype == tl.float32)
-    tl.static_assert(b.dtype == tl.float32)
-    a = a.to(dtype=tl.uint32, bitcast=True).to(tl.uint64)
-    a = a << 32
-    b = b.to(dtype=tl.uint32, bitcast=True).to(tl.uint64)
-    return a | b
-
-
-@triton.jit()
-def first_order_op(l, r):
+def combine1(xl, fl, xr, fr):
     """
-    See https://www.cs.cmu.edu/~guyb/papers/Ble93.pdf Section 1.4.1
+    First order associative op. See https://www.cs.cmu.edu/~guyb/papers/Ble93.pdf Section 1.4.1
     """
-    xl, fl = unpack64(l)
-    xr, fr = unpack64(r)
     x = xl * fr + xr
     f = fl * fr
-    return pack64(x, f)
+    return x, f
 
 
 @triton.jit
@@ -41,17 +18,15 @@ def forward_scan(
     gates,
     tokens,
     outputs,
-    SEQUENCE_LENGTH: tl.constexpr,
+    T: tl.constexpr,
 ):
     sequence_id = tl.num_programs(axis=1) * tl.program_id(axis=0) + tl.program_id(axis=1)
-    strides = tl.arange(0, SEQUENCE_LENGTH) + sequence_id * SEQUENCE_LENGTH
+    strides = tl.arange(0, T) + sequence_id * T
 
     tokens_ = tl.load(tokens + strides)
     gates_ = tl.load(gates + strides)
 
-    tuples = pack64(tokens_, gates_)
-    output_tuples_ = tl.associative_scan(tuples, axis=0, combine_fn=first_order_op)
-    output_tokens_, output_gates_ = unpack64(output_tuples_)
+    output_tokens_, output_gates_ = tl.associative_scan((tokens_, gates_), axis=0, combine_fn=combine1)
     tl.store(outputs + strides, output_tokens_)
 
 
@@ -60,18 +35,16 @@ def backward_scan(
     gates,
     tokens,
     outputs,
-    SEQUENCE_LENGTH: tl.constexpr,
+    T: tl.constexpr,
 ):
     sequence_id = tl.num_programs(axis=1) * tl.program_id(axis=0) + tl.program_id(axis=1)
-    forward_strides = tl.arange(0, SEQUENCE_LENGTH) + sequence_id * SEQUENCE_LENGTH
-    reverse_strides = (tl.num_programs(axis=0) * tl.num_programs(axis=1) * SEQUENCE_LENGTH - 1) - forward_strides
+    forward_strides = tl.arange(0, T) + sequence_id * T
+    reverse_strides = (tl.num_programs(axis=0) * tl.num_programs(axis=1) * T - 1) - forward_strides
 
     tokens_ = tl.load(tokens + reverse_strides)
     gates_ = tl.load(gates + reverse_strides)
 
-    tuples = pack64(tokens_, gates_)
-    output_tuples_ = tl.associative_scan(tuples, axis=0, combine_fn=first_order_op)
-    output_tokens_, output_gates_ = unpack64(output_tuples_)
+    output_tokens_, output_gates_ = tl.associative_scan((tokens_, gates_), axis=0, combine_fn=combine1)
     tl.store(outputs + reverse_strides, output_tokens_)
 
 
@@ -84,7 +57,7 @@ class Scan(torch.autograd.Function):
         assert tokens.is_contiguous()
 
         states = torch.zeros_like(tokens)
-        forward_scan[(B,C)](gates, tokens, states, SEQUENCE_LENGTH=T, enable_fp_fusion=False)
+        forward_scan[(B,C)](gates, tokens, states, T=T, enable_fp_fusion=False)
 
         ctx.save_for_backward(states, gates)
         return states
@@ -102,7 +75,7 @@ class Scan(torch.autograd.Function):
 
         d_states = torch.empty_like(states)
         padded_shifted_gates = torch.cat([gates, torch.ones_like(gates[:, :, :1])], dim=-1)[:, :, 1:].contiguous()
-        backward_scan[(B,C)](padded_shifted_gates, grad_output, d_states, SEQUENCE_LENGTH=T, enable_fp_fusion=False)
+        backward_scan[(B,C)](padded_shifted_gates, grad_output, d_states, T=T, enable_fp_fusion=False)
 
         padded_outputs = torch.cat([torch.zeros_like(states[:, :, :1]), states], dim=-1)[:, :, :-1]
         d_gates = padded_outputs * d_states
